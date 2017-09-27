1eb2ab26eff0c96b345be5bd51e9793cb22a9f2f
aldo <31267265+aldoavila26@users.noreply.github.com>
2017-09-26 23:00:42 -0500

    Update SumNat.R

--- R/SumNat.R
+++ R/SumNat.R
@@ -1,2 +1,10 @@
 sumNat <- function(x)
-{ ((x-1)*(x)/2) }
+{ ((x-1)*(x)/2))}
+
+mean <- function(x) 
+ {sum(x)/length(x)}
+
+geometric<-function(x) 
+  {exp(sum(log(x))/length(x))}
+
+armonic<-1/mean(1/x)
